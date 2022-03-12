7ff13fe34a0f540cb354e7127f2c44e192bc75f8
vu tu <vu.ngoc.tu@alliedtechbase.com>
2022-03-12 22:16:31 +0700

    showing product variations

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Product extends Model implements HasMedia
+{
+    use HasFactory;
+    use InteractsWithMedia;
+
+    public function formattedPrice()
+    {
+        return money($this->price);
+    }
+
+    public function variations()
+    {
+        return $this->hasMany(Variation::class);
+    }
+}
---
+++ app/Models/Variation.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
+
+class Variation extends Model
+{
+    use HasFactory;
+    use HasRecursiveRelationships;
+
+    public function formattedPrice()
+    {
+        return money($this->price);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function inStock()
+    {
+        return $this->stockCount() > 0;
+    }
+
+    public function outOfStock()
+    {
+        return !$this->inStock();
+    }
+
+    public function lowStock()
+    {
+        return !$this->outOfStock() && $this->stockCount() < 5;
+    }
+
+    public function stockCount()
+    {
+        return $this->descendantsAndSelf->sum(fn ($variation) => $variation->stocks->sum('amount'));
+    }
+
+    public function stocks()
+    {
+        return $this->hasMany(Stock::class);
+    }
+}
