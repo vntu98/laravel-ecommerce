961fe5ebaa25b962c1b9eeaad4d43354a8a91609
vu tu <vu.ngoc.tu@alliedtechbase.com>
2022-03-13 13:39:12 +0700

    create cart session

---
+++ app/Cart/Cart.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Cart;
+
+use App\Cart\Contracts\CartInterface;
+use App\Models\Cart as ModelsCart;
+use App\Models\User;
+use Illuminate\Session\SessionManager;
+
+class Cart implements CartInterface
+{
+    protected $instance;
+
+    public function __construct(protected SessionManager $session) { }
+
+    public function exists()
+    {
+        return $this->session->has(config('cart.session.key'));
+    }
+
+    public function create(?User $user = null)
+    {
+        $instance = ModelsCart::make();
+        
+        if ($user) {
+            $instance->user()->associate($user);
+        }
+
+        $instance->save();
+
+        $this->session->put(config('cart.session.key'), $instance->uuid);
+    }
+
+    public function contents()
+    {
+        return $this->instance()->variations;
+    }
+
+    public function contentsCount()
+    {
+        return $this->contents()->count();
+    }
+
+    protected function instance()
+    {
+        if ($this->instance) {
+            return $this->instance;
+        }
+
+        return $this->instance =  ModelsCart::whereUuid($this->session->get(config('cart.session.key')))->first();
+    }
+}
